22f93cc0c84dcc2a7ee99de018ff6e389a3b8fe2
nadine <chernenkovans@mail.ru>
2023-08-19 23:41:09 +0300

    new structure

--- bot/handlers.py
+++ bot/handlers.py
@@ -8,22 +8,15 @@ from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram import types
 
-from bot_answers import *
+from bot.bot_answers import *
 from user_answers import *
 
-token="6412831335:AAGBAvIVJKzY90o9-hdDbQoQR7elyJ1WQcs"
-selfie_id='AgACAgIAAxkBAAPfZOEkW6-UBji0Aour7hqt26LfxY4AAjDLMRtWXglLEaOkTj1VPnkBAAMCAANzAAMwBA'
-school_id='AgACAgIAAxkBAAPgZOEkcxEbNG1iHxlJlC5WyZ5Ht0oAArHLMRvRbAlLkreWOYy5eTcBAAMCAANzAAMwBA'
-
-voice_gpt_id='AwACAgIAAxkBAAOEZODyIf6L9339drVxRYkuzPlNxnYAAsg0AALRbAFL-42ObnXdRIAwBA'
-voice_sql_id='AwACAgIAAxkBAAOGZOENz0Wd8k7gRe09Bvk2Az3q7KQAArUyAAJWXglL5MYG0JKGbb4wBA'
-voice_love_id='AwACAgIAAxkBAAOFZOEGGWOaq0hdqNIgJ6CjWGuigHcAAoYyAAJWXglLqBaSfI3X19YwBA'
-# token = os.getenv("TOKEN")
-# selfie_id = os.getenv("SELFIE_ID")
-# school_id = os.getenv("SCHOOL_ID")
-# voice_gpt_id = os.getenv("VOICE_GPT_ID")
-# voice_sql_id = os.getenv("VOICE_SQL_ID")
-# voice_love_id = os.getenv("VOICE_LOVE_ID")
+token = os.getenv("TOKEN")
+selfie_id = os.getenv("SELFIE_ID")
+school_id = os.getenv("SCHOOL_ID")
+voice_gpt_id = os.getenv("VOICE_GPT_ID")
+voice_sql_id = os.getenv("VOICE_SQL_ID")
+voice_love_id = os.getenv("VOICE_LOVE_ID")
 
 
 router = Router()
@@ -96,14 +89,10 @@ async def process_callback_button1(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     await bot.send_voice(callback_query.from_user.id, voice=voice_love_id)
 
-# @router.message()
-# async def scan_message(msg: types.Message):
-#     document_id = msg.photo[0].file_id
-#     file_info = await bot.get_file(document_id)
-#     print(f'file_id: {file_info.file_id}')
-#     print(f'file_path: {file_info.file_path}')
-#     print(f'file_size: {file_info.file_size}')
-#     print(f'file_unique_id: {file_info.file_unique_id}')
+
+@router.message(Command("sourse_code"))
+async def send_selfie(message: types.Message):
+    await message.answer(sourse_link)
 
 
 async def main() -> None:
